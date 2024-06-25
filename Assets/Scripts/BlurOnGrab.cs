42bde9d759bb894f909deaa288afc7a3326c29e9
toka404 <bryan97_flores97@hotmail.com>
2024-06-24 20:38:42 -0500

    Commit Inicial

---
+++ Assets/Scripts/BlurOnGrab.cs
@@ -0,0 +1,43 @@
+using Oculus.Interaction;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class BlurOnGrab : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject padre;
+    [SerializeField]
+    private Volume blur;
+
+
+    int blurLayer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blurLayer = LayerMask.NameToLayer("NoBlur");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnGrab()
+    {
+        // Check if random chance is set
+        padre.layer = blurLayer;
+        blur.weight = 0.9f;
+    }
+
+    public void OnRelease()
+    {
+        // Check if random chance is set
+        padre.layer = 0;
+        blur.weight = 0;
+    }
+}
